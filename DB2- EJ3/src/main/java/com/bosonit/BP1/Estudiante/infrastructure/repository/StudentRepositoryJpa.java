e9be6f24727423125bc9a95f9729031fe7448d90
Vicente Cantero Luna <108876969+VicenteCanteroLuna@users.noreply.github.com>
2022-07-26 07:55:20 +0200

    Delete DB2- EJ3 directory

--- DB2- EJ3/src/main/java/com/bosonit/BP1/Estudiante/infrastructure/repository/StudentRepositoryJpa.java
+++
@@ -1,7 +0,0 @@
-package com.bosonit.BP1.Estudiante.infrastructure.repository;
-
-import com.bosonit.BP1.Estudiante.Domain.Student;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StudentRepositoryJpa extends JpaRepository<Student, String> {
-}
--- DB2- EJ3/src/main/java/com/bosonit/BP1/Profesor/infrastructure/repository/ProfesorRepositoryJpa.java
+++
@@ -1,7 +0,0 @@
-package com.bosonit.BP1.Profesor.infrastructure.repository;
-
-import com.bosonit.BP1.Profesor.domain.Profesor;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProfesorRepositoryJpa extends JpaRepository<Profesor, String> {
-}
